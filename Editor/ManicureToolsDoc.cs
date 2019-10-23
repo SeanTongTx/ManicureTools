e6d34b907788ce48adecac1bb37285ec585b0bb3
Seantong <jacquestx1@163.com>
2019-10-23 17:05:16 +0800

    First Version

---
+++ Editor/ManicureToolsDoc.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EditorPlus;
+[CustomSeanLibEditor("ManicureTools")]
+public class ManicureToolsDoc : EditorMarkDownWindow
+{
+
+}
